18c55c67d819c2072f1838d215516a7e864c6a2d
Uriel Katz <uriel.katz@gmail.com>
2011-03-26 16:40:00 +0200

    new setup.py using setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Shaveet', 
       version='0.1', 
       packages=['shaveet'],
       package_data={'shaveet': ['static/*.js']},
-      )
+      install_requires = ['gevent>=0.13','python-daemon','wsgi-jsonrpc','static'],
+      extras_require = {'process title':  ["setproctitle"]},
+      author = "Uriel Katz",
+      author_email = "uriel.katz@gmail.com",
+      description = "Shaveet comet server",
+      license = "MIT",
+      keywords = "comet gevent server",
+      url = "https://github.com/urielka/shaveet",
+      zip_safe = False
+)
